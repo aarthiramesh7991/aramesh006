4ca78421149ad9ac4a95abbcd3acee2b9cf1b5fd
aarthiramesh7991 <aarthiramesh7991@gmail.com>
2022-12-12 10:19:05 +0530

    initial code

---
+++ src/org/tec/GitTesting.java
@@ -0,0 +1,14 @@
+package org.tec;
+
+public class GitTesting {
+	private void test1() {
+		System.out.println("Test 1");}
+			
+			private void test2() {
+				System.out.println("Test 2");}
+			
+			private void test3() {
+				System.out.println("Test 3");}
+			
+
+}
